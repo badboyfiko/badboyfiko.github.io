5b2f1b0f8e41b77299dadebdf37834113e3a7e8d
Fiko <fiko@vestberry.com>
2019-05-12 02:18:28 +0200

    Updates

---
+++ precache-manifest.91c28db8d53a70c0c6317b07da736620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616a979007decd4c6b561abe0e9f86f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71560bc12f12a17e1cb1",
+    "url": "/static/css/main.15bf3002.chunk.css"
+  },
+  {
+    "revision": "9b3c20745204cdc0d592",
+    "url": "/static/js/2.396feccc.chunk.js"
+  },
+  {
+    "revision": "71560bc12f12a17e1cb1",
+    "url": "/static/js/main.e45b442e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de7019d04ad2eb90729523906356aae2",
+    "url": "/static/media/ico_ui_arrow.de7019d0.svg"
+  }
+]);
